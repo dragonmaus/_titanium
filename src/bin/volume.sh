1d27851e576abf06184b23c0db6dc365295d9d89
David Wildasin <dragonmaus@posteo.net>
2019-08-21 13:34:33 -0400

    Rewrite volume script

--- src/bin/volume.sh
+++ src/bin/volume.sh
@@ -1,34 +1,51 @@
 #!/bin/sh
 set -e
 
-min=0
-max=255
 inc=5
+max=255
+
+getlevel() {
+	print $(($(mixerctl -n outputs.master | cut -d , -f 1) * 100))
+}
+level2percent() {
+	print $((level / max))
+}
+percent2level() {
+	print $((percent * max))
+}
+printlevel() {
+	print "$percent% ($((level / 100))/$max)"
+}
+roundlevel() {
+	print $(((level + ((100 - (level % 100)) % 100)) / 100))
+}
+roundpercent() {
+	print $((percent + ((inc - (percent % inc)) % inc)))
+}
 
 case "$*" in
 ('')
-	level=$(mixerctl -n outputs.master | cut -d , -f 1)
-	percent=$((level * 100 / max))
-	print "$percent% ($level/$max)"
+	level=$(getlevel)
+	percent=$(level2percent)
+	printlevel
 	exit 0
 	;;
 (+|-)
-	level=$(mixerctl -n outputs.master | cut -d , -f 1)
-	level=$((level * 100 / max))
-	level=$((level + ((inc - (level % inc)) % inc)))
-	level=$((level $1 inc))
-	[[ $level -lt 0 ]] && level=0
-	[[ $level -gt 100 ]] && level=100
-	level=$((max * level / 100))
-	print "$((level * 100 / max))% ($level/$max)"
-	exec mixerctl -q outputs.master=$level,$level
+	level=$(getlevel)
+	percent=$(level2percent)
+	percent=$(roundpercent)
+	percent=$((percent $1 inc))
+	[[ $percent -lt 0 ]] && percent=0
+	[[ $percent -gt 100 ]] && percent=100
+	level=$(percent2level)
+	printlevel
+	exec mixerctl -q outputs.master=$(roundlevel)
 	;;
 ([0-9]|[1-9][0-9]|100)
-	level=$(($1))
-	# level=$((level + ((inc - (level % inc)) % inc)))
-	level=$((max * level / 100))
-	print "$((level * 100 / max))% ($level/$max)"
-	exec mixerctl -q outputs.master=$level,$level
+	percent=$(($1))
+	level=$(percent2level)
+	printlevel
+	exec mixerctl -q outputs.master=$(roundlevel)
 	;;
 (mute)
 	exec mixerctl -q outputs.master.mute=toggle
