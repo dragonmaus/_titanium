06a6379f0dd87c55abfa1847a06395376e3ab84d
David Wildasin <dragonmaus@posteo.net>
2020-02-17 09:33:41 -0500

    update: Keep python packages updated

---
+++ etc/update.d/41-python.sh
@@ -0,0 +1,8 @@
+(
+  for i in 2 3
+  do
+    echo ">> Updating python$i packages"
+    pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user pip
+    pip$i list --format=json --no-python-version-warning --not-required --user | jq -r '.[] | .name' | grep -Fvx pip | xargs -r pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user
+  done
+)
