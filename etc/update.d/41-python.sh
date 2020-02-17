0415952ba8c00a29c51653057797dfeb0b72178a
David Wildasin <dragonmaus@posteo.net>
2020-02-17 10:59:57 -0500

    update: Silence python package updates

--- etc/update.d/41-python.sh
+++ etc/update.d/41-python.sh
@@ -2,7 +2,7 @@
   for i in 2 3
   do
     echo ">> Updating python$i packages"
-    pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user pip
-    pip$i list --format=json --no-python-version-warning --not-required --user | jq -r '.[] | .name' | grep -Fvx pip | xargs -r pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user
+    pip$i install -Uq --no-python-version-warning --upgrade-strategy=eager --user pip
+    pip$i list --format=json --no-python-version-warning --not-required --user | jq -r '.[] | .name' | grep -Fvx pip | xargs -r pip$i install -Uq --no-python-version-warning --upgrade-strategy=eager --user
   done
 )
