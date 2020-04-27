a652f2963bf5a0aaae5e1deba52e732dd01eae82
David Wildasin <dragonmaus@posteo.net>
2020-04-27 10:30:34 -0400

    Fix architecture detection

--- etc/update.d/12-packages.sh
+++ etc/update.d/12-packages.sh
@@ -1,5 +1,5 @@
 echo '>> Updating packages'
 doas env \
   PKG_PATH=./:installpath \
-  TRUSTED_PKG_PATH=/home/ports/packages/$(uname -m)/all \
+  TRUSTED_PKG_PATH=/home/ports/packages/`arch -s`/all \
   pkg_add -u || :
