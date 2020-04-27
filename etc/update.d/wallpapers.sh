0a82d6d92ba5bb4f231102cf8b9ceb72df84e43c
David Wildasin <dragonmaus@posteo.net>
2020-04-27 14:06:50 -0400

    Prefer openrsync over rsync

--- etc/update.d/wallpapers.sh
+++ etc/update.d/wallpapers.sh
@@ -1,2 +1,2 @@
 echo '>> Updating wallpapers'
-rsync -av --delete-during home:$(ssh home xdg-user-dir BACKGROUNDS)/8:5/ $(xdg-user-dir BACKGROUNDS)/8:5
+openrsync -av --del home:$(ssh home xdg-user-dir BACKGROUNDS)/8:5/ $(xdg-user-dir BACKGROUNDS)/8:5
