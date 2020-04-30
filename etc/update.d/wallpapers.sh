93e1de3dc949032accdd384addf638fc7116053a
David Wildasin <dragonmaus@posteo.net>
2020-04-30 09:28:01 -0400

    Fix quoting

--- etc/update.d/wallpapers.sh
+++ etc/update.d/wallpapers.sh
@@ -1,2 +1,2 @@
 echo '>> Updating wallpapers'
-openrsync -av --del home:"$(ssh home xdg-user-dir BACKGROUNDS)/1.6 (8:5)/" "$(xdg-user-dir BACKGROUNDS)/1.6 (8:5)"
+openrsync -av --del "home:'$(ssh home xdg-user-dir BACKGROUNDS)/1.6 (8:5)/'" "$(xdg-user-dir BACKGROUNDS)/1.6 (8:5)"
