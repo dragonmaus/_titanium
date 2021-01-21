04de4f4d18e7b46aabf46e56c89b5fc19054c6b3
David Wildasin <dragonmaus@posteo.net>
2021-01-21 14:51:05 -0500

    Update update scripts

--- etc/update.d/cvs.sh
+++ etc/update.d/cvs.sh
@@ -2,7 +2,7 @@ echo '>> Updating CVS repositories'
 r=$(uname -r | tr . _)
 for repo in /usr/{src,xenocara,ports}
 do
-  [[ -d $repo ]] || continue
-  echo - $repo
-  (cd $repo && exec cvs -q update -Pd -r "OPENBSD_$r")
+	[ -d $repo ] || continue
+	echo - $repo
+	(cd $repo && exec cvs -q update -Pd -r "OPENBSD_$r")
 done
