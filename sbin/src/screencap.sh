8f08f22666f3e19bd55ea79fd3c4e6eae2b0a4f3
David Wildasin <dragonmaus@posteo.net>
2021-01-21 22:25:56 -0500

    Update screencap

--- sbin/src/screencap.sh
+++ sbin/src/screencap.sh
@@ -11,4 +11,4 @@ do
 done
 shift $((OPTIND - 1))
 
-exec gm import ${window:+-window "$window"} "$@" ~/desk/pictures/screenshots/$(date -u +%FT%TZ).png
+exec gm import ${window:+-window "$window"} "$@" "$(xdg-user-dir SCREENSHOTS)/$(date -u +%FT%TZ).png"
