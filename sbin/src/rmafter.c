d5334b7258347e745fe6dd12335e0bc56f772e81
David Wildasin <dragonmaus@posteo.net>
2021-01-26 19:53:28 -0500

    sbin/rmafter: Use existing functionality where possible

--- sbin/src/rmafter.c
+++ sbin/src/rmafter.c
@@ -1,7 +1,7 @@
-#include <errno.h>
+#include <err.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/event.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -11,10 +11,6 @@
 #define PID_MAX 99999
 #endif
 
-void die1(int, const char *);
-void err_die1(int, const char *);
-void err_die2(int, const char *, const char *);
-
 	int
 main(int argc, char **argv)
 {
@@ -22,18 +18,20 @@ main(int argc, char **argv)
 	struct kevent ev;
 	pid_t pid;
 	int kq, nev, i;
-	const char *err;
+	const char *errstr;
 
 	if (argc < 3) {
-		die1(1, "Usage: rmafter pid file [file ...]");
+		fprintf(stderr, "Usage: %s pid file [file ...]\n", basename(*argv));
+		return EXIT_FAILURE;
 	}
-	pid = (pid_t)strtonum(argv[1], 0, PID_MAX, &err);
-	if (err) {
-		err_die1(1, "rmafter: strtonum() error");
+
+	pid = (pid_t)strtonum(argv[1], 0, PID_MAX, &errstr);
+	if (errstr) {
+		err(EXIT_FAILURE, "strtonum() error: %s", errstr);
 	}
 
 	if ((kq = kqueue()) == -1) {
-		err_die1(1, "rmafter: kqueue() error");
+		err(EXIT_FAILURE, "kqueue() error");
 	}
 
 	EV_SET(&ch, pid, EVFILT_PROC, EV_ADD | EV_ENABLE, NOTE_EXIT, 0, 0);
@@ -41,42 +39,23 @@ main(int argc, char **argv)
 	for (;;) {
 		nev = kevent(kq, &ch, 1, &ev, 1, NULL);
 		if (nev < 0) {
-			err_die1(1, "rmafter: kevent() error");
+			err(EXIT_FAILURE, "kevent() error");
 		}
 		if (nev > 0) {
 			if (ev.flags & EV_ERROR) {
-				err_die2(1, "rmafter", argv[1]);
+				err(EXIT_FAILURE, "%s", argv[1]);
 			}
 			break;
 		}
 	}
 
 	close(kq);
+
 	for (i = 2; i < argc; ++i) {
 		if (!unlink(argv[i])) {
 			printf("Removed file '%s'\n", argv[i]);
 		}
 	}
+
 	return EXIT_SUCCESS;
-}
-
-	void
-die1(int r, const char *s)
-{
-	fprintf(stderr, "%s\n", s);
-	exit(r);
-}
-
-	void
-err_die1(int r, const char *s)
-{
-	fprintf(stderr, "%s: %s\n", s, strerror(errno));
-	exit(r);
-}
-
-	void
-err_die2(int r, const char *s, const char *t)
-{
-	fprintf(stderr, "%s: %s: %s\n", s, t, strerror(errno));
-	exit(r);
 }
