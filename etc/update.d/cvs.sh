cbdf97b8c26e3621a0f8704e7424ccc62e16c679
David Wildasin <dragonmaus@posteo.net>
2021-02-04 10:52:47 -0500

    Reference local mirror when updating CVS repos

--- etc/update.d/cvs.sh
+++ etc/update.d/cvs.sh
@@ -1,8 +1,9 @@
 echo '>> Updating CVS repositories'
+read -r cvsroot < ~/etc/cvsroot
 r=$(uname -r | tr . _)
 for repo in /usr/{src,xenocara,ports}
 do
 	[ -d $repo ] || continue
 	echo - $repo
-	(cd $repo && exec cvs -q update -Pd -r OPENBSD_$r)
+	(cd $repo && exec cvs -q -d $cvsroot update -Pd -r OPENBSD_$r)
 done
